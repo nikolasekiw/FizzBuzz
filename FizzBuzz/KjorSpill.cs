ff4dd9745f2af9fe7f8376f6a8b7762d69526d58
Thomas Leela <thomas.leela@epinova.no>
2020-12-16 18:30:48 +0100

    Typo in comment

--- FizzBuzz/KjorSpill.cs
+++ FizzBuzz/KjorSpill.cs
@@ -8,9 +8,9 @@
             fb.StartSpill();
 
             var jf = new Jazz_Fuzz();
-jf.regler.Add(4, "Fuzz");
-jf.regler.Add(9, "Jazz");
-jf.regler.Add(36, "JazzFuzz");
+            jf.regler.Add(4, "Fuzz");
+            jf.regler.Add(9, "Jazz");
+
             //Denne kombinasjonsregelen vil jeg at du fjerner ved å skrive om koden din
             //De varslede fremtidige endringene kan f.eks være utvidelse av spillet med to nye regler slik:
             //2-Foo, 4-Fuzz, 9-Jazz, 11-Bar
@@ -18,8 +18,7 @@ jf.regler.Add(36, "JazzFuzz");
             //lage alle de 14 nye kombinasjonsreglene. Et slikt sceanrio er også skjørt og utsatt for menneskelig feil.
             //Man kan også argumentere for at kombimasjoneregler er DRY/WET violation fordi du repeterer noe du allerede har gjort før.
             //Dersom verdien for Fuzz skal endres til f.eks 5, av en annen utvikler enn deg, er det en mulighet 36-FizzBuzz feilaktig forblir stående urørt.
-
-
+            jf.regler.Add(36, "JazzFuzz");
             jf.StartSpill();
         }
     }
