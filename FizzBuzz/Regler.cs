254646001f070eb734eac58ed58a855e34b86c54
Thomas Leela <thomas.leela@epinova.no>
2020-12-21 14:41:46 +0100

    Remove unnecessary names on tuplets

--- FizzBuzz/Regler.cs
+++ FizzBuzz/Regler.cs
@@ -2,7 +2,7 @@
 {
     public record Regler
     {
-        public static (int Divident, string Navn)[] FizzBuzz => new[] { (3, "Fizz"), (5, "Buzz") };
-        public static (int Divident, string Navn)[] JazzFuzz => new[] { (9, "Jazz"), (4, "Fuzz") };
+        public static (int, string)[] FizzBuzz => new[] { (3, "Fizz"), (5, "Buzz") };
+        public static (int, string)[] JazzFuzz => new[] { (9, "Jazz"), (4, "Fuzz") };
     }
 }
